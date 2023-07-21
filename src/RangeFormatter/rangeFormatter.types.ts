96ae3b889c77495a84cc7f4202b087c71e8b2fae
Aleks-kap1009 <trololo1009@gmail.com>
2023-07-21 15:06:07 +0600

    implement a rangeFormatter function

---
+++ src/RangeFormatter/rangeFormatter.types.ts
@@ -0,0 +1,8 @@
+export type TStyle = 'when' | 'after';
+export type TWidth = 'full' | 'shortest' | 'short';
+
+export interface RangeFormatterOpt {
+  locale?: string | string[] | undefined;
+  futureStyle?: TStyle | undefined;
+  width?: TWidth | undefined;
+}
